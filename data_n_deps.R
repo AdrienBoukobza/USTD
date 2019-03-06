86fe06200c9a126e2a7a0f34f7d1ac8ef9c2dbc7
Maxime Wack <maximewack@free.Fr>
2019-03-07 00:23:24 +0100

    Add comments

--- data_n_deps.R
+++ data_n_deps.R
@@ -11,14 +11,18 @@ library(prophet)
 library(shinycustomloader)
 library(scales)
 
+# Load states boundaries
 states <- us_states()
 
+# Load CDC data
 STD = readRDS("data/STD.rds") %>% rename(Ethnicity = `Race/Ethnicity`)
 
+# Create indexes for the selectors
 STD %>%
   select(Disease, Gender, Age, Ethnicity, State) %>%
   map(~ unique(.) %>% sort) -> selectVars
 
+# Function to add "All" to a vector
 andAll <- function(x)
   c("All", x)
 
